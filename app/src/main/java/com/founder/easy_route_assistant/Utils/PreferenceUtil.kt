ac9778cfe0870bf4918fd660a833a1f865740499
JIEUNI <jieundaeun@naver.com>
2023-11-23 20:05:36 +0900

    [add] #2 SharedPreference token 저장 구현

---
+++ app/src/main/java/com/founder/easy_route_assistant/Utils/PreferenceUtil.kt
@@ -0,0 +1,17 @@
+package com.founder.easy_route_assistant.Utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceUtil(context: Context) {
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
+
+    fun getString(key: String, defValue: String): String{
+        return prefs.getString(key, defValue).toString()
+    }
+
+    fun setString(key:String, str:String){
+        prefs.edit().putString(key, str).apply()
+    }
+}
