e28b3acf1beb4f74ed9619338d3a9ee5f3ee46c4
JIEUNI <jieundaeun@naver.com>
2023-11-28 11:04:26 +0900

    [chore] #5 merge conflict 해결

--- app/src/main/java/com/founder/easy_route_assistant/presentation/ListLayout.kt
+++ app/src/main/java/com/founder/easy_route_assistant/presentation/ListLayout.kt
@@ -4,6 +4,7 @@ package com.founder.easy_route_assistant.presentation
 class ListLayout(
     val name: String, // 장소명
     val road: String, // 도로명 주소
+    val number: String,
     val x: Double, // 경도(Longitude)
     val y: Double,
 ) // 위도(Latitude)
