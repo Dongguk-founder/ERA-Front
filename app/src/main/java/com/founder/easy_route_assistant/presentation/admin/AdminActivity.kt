1926e12b198201b8467ac960f9f23288be478630
JIEUNI <jieundaeun@naver.com>
2023-11-28 13:51:09 +0900

    [feat] #8 관리자 뷰 리사이클러뷰 아이템 구현

---
+++ app/src/main/java/com/founder/easy_route_assistant/presentation/admin/AdminActivity.kt
@@ -0,0 +1,12 @@
+package com.founder.easy_route_assistant.presentation.admin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.founder.easy_route_assistant.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
